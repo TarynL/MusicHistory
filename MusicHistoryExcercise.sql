7b9522080833868152dccd6ae8a664e6c9ffc903
TarynL <tarlytle@gmail.com>
2021-06-03 08:58:51 -0500

    step 10 complete

---
+++ MusicHistoryExcercise.sql
@@ -0,0 +1,18 @@
+﻿SELECT
+	Id,
+	Name
+From Genre;
+
+SELECT
+	id,
+	artistName,
+	yearEstablished
+FROM Artist
+ORDER BY ArtistName;
+
+SELECT
+	s.title,
+	a.artistName
+From Song s LEFT JOIN Artist a  on s.title = a.id;
+
+
---
+++ MusicHistoryIntro.sql
@@ -0,0 +1,12 @@
+﻿SELECT s.Title,
+       a.ArtistName
+  FROM Song s
+       LEFT JOIN Artist a on s.ArtistId = a.id;
+
+SELECT * FROM genre;
+     
+INSERT INTO Genre (Name) VALUES ('Techno');
+
+update genre
+set name = 'Folk'
+where Id = 1;
